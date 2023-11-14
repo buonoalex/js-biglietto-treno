e86584b707f4e7e8d345a948e876f6f93930160d
buonoalex <loopatthecity@gmail.com>
2023-11-14 09:35:06 +0100

    add function person count

--- js/main.js
+++ js/main.js
@@ -1,38 +1,47 @@
 // Questo è il file che contiene tutto il codice JS della pagina web
 
 //Prompt età + costante
-let agePassenger = parseInt(prompt("Inserisci la tua età:"));
+let persontravel = parseInt(prompt("Quante persone viaggiano:"));
 let kmTravel = parseInt(prompt("Dimmi quanti km devi percorrere:"));
 const PrizeKm = 0.21;
+
 let finalPrize;
 
-switch(true){
+let totalprize = 0;
 
-    case(agePassenger > 18 && agePassenger < 65):
-    finalPrize = PrizeKm * kmTravel;
-    parseFloat(finalPrize);
-    document.getElementById("risultatoSomma").innerHTML = "Ecco il conto da pagare "+finalPrize.toFixed(2)+"£";
-    console.log("Passeggero Normale")
-    break;
+for (let i =0 ; i<persontravel; i++){
 
-    case(agePassenger < 18):
-    let finalPrizeDiscount20;
-    finalPrize = PrizeKm * kmTravel;
-    finalPrizeDiscount20 = finalPrize - ((finalPrize * 20) / 100);
-    parseFloat(finalPrize);
-    document.getElementById("risultatoSomma").innerHTML = "Ecco il conto da pagare "+finalPrizeDiscount20.toFixed(2)+"£";
-    console.log("Passeggero Minorenne")
-    break;
+    let agePassenger = parseInt(prompt("Inserisci la tua età:"));
 
-    case(agePassenger > 65):
-    let finalPrizeDiscount40;
-    finalPrize = PrizeKm * kmTravel;
-    finalPrizeDiscount40 = finalPrize - ((finalPrize * 40) / 100);
-    parseFloat(finalPrize);
-    document.getElementById("risultatoSomma").innerHTML = "Ecco il conto da pagare "+finalPrizeDiscount40.toFixed(2)+"£";
-    console.log("Passeggero Ansiano")
-    break;
+    switch(true){
 
-    
+        case(agePassenger > 18 && agePassenger < 65):
+        finalPrize = PrizeKm * kmTravel;
+        parseFloat(finalPrize);
+        totalprize = totalprize + finalPrize;
+        console.log("Passeggero Normale")
+        break;
+
+        case(agePassenger < 18):
+        let finalPrizeDiscount20;
+        finalPrize = PrizeKm * kmTravel;
+        finalPrizeDiscount20 = finalPrize - ((finalPrize * 20) / 100);
+        parseFloat(finalPrize);
+        totalprize = totalprize + finalPrizeDiscount20;
+        console.log("Passeggero Minorenne")
+        break;
+
+        case(agePassenger > 65):
+        let finalPrizeDiscount40;
+        finalPrize = PrizeKm * kmTravel;
+        finalPrizeDiscount40 = finalPrize - ((finalPrize * 40) / 100);
+        parseFloat(finalPrize);
+        totalprize = totalprize + finalPrizeDiscount40;
+        console.log("Passeggero Ansiano")
+        break;
+
+    }
 
 }
+
+document.getElementById("risultatoSomma").innerHTML = totalprize.toFixed(2)+"$";
